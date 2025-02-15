c1dde360283b0cebe65e4f2468265137946bb9eb
Shoutcape <kautiainen.ville@gmail.com>
2025-02-15 16:42:13 +0200

    added cors

--- index.ts
+++ index.ts
@@ -3,10 +3,12 @@ const app = express();
 import workoutsRouter from './controllers/workouts'
 import morgan from 'morgan';
 import config from './utils/config';
+import cors from 'cors'
 
 // Middleware
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(cors())
 
 //Routes
 app.use("/", workoutsRouter)
